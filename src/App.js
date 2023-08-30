3445f3c2d34b9c93bba83d2512668fcd8564aa02
Deni Shaipov <deni.shaipp@gmail.com>
2023-08-30 14:51:25 +0300

    another commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
+  async function netlifyTest() {
+    let response = await fetch('/.netlify/functions/test');
+    let result = await response.json();
+
+    console.log('useEffect firing');
+    console.log(result);
+  }
+  useEffect(() => {
+    netlifyTest();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={netlifyTest}></button>
     </div>
   );
 }
